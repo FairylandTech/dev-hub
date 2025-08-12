1bf53aa502979347123c6944e6b686a92b1f3481
Lionel Johnson <fairylandfuture@protonmail.com>
2025-08-12 21:45:28 +0800

    :sparkles: Java code

---
+++ codes/java/playground/playground-01/learn-02-loop/src/main/java/org/example/loop/demo/Demo7.java
@@ -0,0 +1,21 @@
+/*****************************************************
+ * @software: IntelliJ IDEA
+ * @author: Lionel Johnson
+ * @contact: https://fairy.host
+ * @organization: https://github.com/FairylandFuture
+ * @datetime: 2025-08-12 21:40:11 UTC+08:00
+ ****************************************************/
+package org.example.loop.demo;
+
+/**
+ * @author Lionel Johnson
+ */
+public class Demo7 {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
